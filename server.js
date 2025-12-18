c7a35fdfa0c5909d10c7e49d122fd1a72bfb5675
guilhermetft <guiftf2007@gmail.com>
2025-12-17 21:41:33 -0300

    url front

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://2025-2-p2-tiapn-webdealer.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
